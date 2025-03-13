8ec3c76702e8d54d78df577be3914ec6cdca74f9
Farnoosh Moayeri <farnooshmoayeri@gmail.com>
2025-03-13 16:19:39 +0000

    create CRUD routes

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -7,6 +7,10 @@ import javax.inject._
 @Singleton
 class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
   def index(): Action[AnyContent] = TODO
+  def create(): Action[AnyContent] = TODO
+  def read(id: String): Action[AnyContent] = TODO
+  def update(id: String): Action[AnyContent] = TODO
+  def delete(id: String): Action[AnyContent] = TODO
 }
 
 

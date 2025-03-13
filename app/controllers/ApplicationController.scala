f44cc8de5b240e9b501206fab511dded18b5f9b2
Nimo Mohamed <138133949+nimo-mohamed@users.noreply.github.com>
2025-03-13 17:46:34 +0000

    made index test pass

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -3,10 +3,13 @@ package controllers
 import play.api.mvc._
 
 import javax.inject._
+import scala.concurrent.Future
 
 @Singleton
 class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-  def index(): Action[AnyContent] = TODO
+  def index(): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
+    Future.successful(Ok(views.html.index()))
+  }
   def create(): Action[AnyContent] = TODO
   def read(id: String): Action[AnyContent] = TODO
   def update(id: String): Action[AnyContent] = TODO

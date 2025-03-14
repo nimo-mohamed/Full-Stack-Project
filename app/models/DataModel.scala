68ccd7573176d3390cb6207fd449b4d5be33821d
Nimo Mohamed <138133949+nimo-mohamed@users.noreply.github.com>
2025-03-14 10:50:01 +0000

    created dataRepository

--- app/models/DataModel.scala
+++ app/models/DataModel.scala
@@ -4,6 +4,10 @@ import play.api.libs.json.{Json, OFormat}
 
 case class DataModel(_id: String, name: String, description: String, pageCount: Int)
 
+
 object DataModel {
   implicit val formats: OFormat[DataModel] = Json.format[DataModel]
+
+//  val bookOne = DataModel("id1", "Book name", "Author name", 10)
+//  println(bookOne.name)
 }

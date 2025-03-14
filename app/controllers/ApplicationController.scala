4d2a1df63ba2250b6c921374033f9b49efbfa6b0
Nimo Mohamed <138133949+nimo-mohamed@users.noreply.github.com>
2025-03-14 11:17:02 +0000

    inject DataRepositor and EC as dependecy

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,12 +1,13 @@
 package controllers
 
 import play.api.mvc._
+import repositories.DataRepository
 
 import javax.inject._
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class ApplicationController @Inject()(val controllerComponents: ControllerComponents, val repository: DataRepository, implicit val ec: ExecutionContext) extends BaseController {
   def index(): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
     Future.successful(Ok(views.html.index()))
   }
